4c552d390b5b4d0e6e3649c579013dec6bfe5ea9
rdopazo <reuben@reluscloud.com>
2017-09-21 14:36:18 -0300

    a

--- vars/aemBuild.groovy
+++ vars/aemBuild.groovy
@@ -1,19 +1,4 @@
-def call() {
+def call(String name='nombre') {
     
-    def buildInfo = Artifactory.newBuildInfo()
-    buildInfo.env.capture = true
-    def server = Artifactory.server 'artifactory-server'
-    rtMaven = Artifactory.newMavenBuild()
-    rtMaven.tool = 'M3' // Tool name from Jenkins configuration
-    rtMaven.deployer releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local', server: server
-    rtMaven.resolver releaseRepo: 'libs-releases', snapshotRepo: 'libs-snapshot', server: server
-
-
-
-
-
-    // now build, based on the configuration provided
-    stage (build) {
-        rtMaven.run pom: 'pom.xml', goals: 'clean test'
-    }
+    echo "Hello, ${name}"
 }
